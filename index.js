ca6bba9abc938fb324a43829825c6595b3aba2da
LegoDev <43821940+LegoDevStudio@users.noreply.github.com>
2021-05-19 11:29:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./Console.js");
 const io = require('socket.io')(3007);
 const tmi = require('tmi.js');
 
-const client = new tmi.Client({
+/*const client = new tmi.Client({
 	identity: {
 		username: process.env.USERNAME,
 		password:	process.env.TOKEN
@@ -23,13 +23,17 @@ client.on("cheer", (channel, userstate, message) => {
 	// Cheer recieved. Get bits then forward to plugin.
 	console.info("Recieved cheer in "+channel.trim().replace("#","")+" by "+userstate["display-name"]+" of "+userstate.bits+" bits.");
 	io.emit("donation", userstate.bits);
-});
+});*/
 
 io.on("connection", (socket) => {
 	// Connection recieved.
+	console.info("Recieved connection from (hopefully) the plugin.");
 	socket.on("chat", message => {
+		console.debug("Plugin trying to send message. Verifying that we're allowed to");
+		console.debug("Incoming message content: "+message);
 		if(process.env.CANCHAT == "yes") {
-			client.send(process.env.TARGET, message);
+			console.debug("We're verified to do so. Sending message.");
+			//client.send(process.env.TARGET, message);
 		}
 	});
 });
