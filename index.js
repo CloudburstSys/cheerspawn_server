43cabe47756d008a1dfb08cc7e49b64682f67ba5
LegoDev <43821940+LegoDevStudio@users.noreply.github.com>
2021-05-19 17:49:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
+
 
 /*const client = new tmi.Client({
 	identity: {
@@ -27,24 +28,28 @@ client.connect();
 client.on("cheer", (channel, userstate, message) => {
 	// Cheer recieved. Get bits then forward to plugin.
 	console.info("Recieved cheer in "+channel.trim().replace("#","")+" by "+userstate["display-name"]+" of "+userstate.bits+" bits.");
-	io.emit("donation", userstate.bits);
+	wss.clients.forEach(function each(client) {
+      		if (client.readyState === WebSocket.OPEN) {
+        		client.send(userstate.bits);
+      		}
+    	});
 });*/
 
 app.get("/", (req,res) => {
 	res.status(200).json({"ok":true});	
 });
 
-io.on("connection", (socket) => {
+wss.on('connection', function connection(ws) {
 	// Connection recieved.
 	console.info("Recieved connection from (hopefully) the plugin.");
-	socket.on("chat", message => {
-		console.debug("Plugin trying to send message. Verifying that we're allowed to");
+	ws.on('message', function incoming(message) {
+    		console.debug("Plugin trying to send message. Verifying that we're allowed to");
 		console.debug("Incoming message content: "+message);
 		if(process.env.CANCHAT == "yes") {
 			console.debug("We're verified to do so. Sending message.");
 			//client.send(process.env.TARGET, message);
 		}
-	});
+  	});
 });
 
 server.listen(3007, () => {
